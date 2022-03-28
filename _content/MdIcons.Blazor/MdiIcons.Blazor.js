664d120a04e43ffc48802273f73606f65671f090
davidnajar <davidnh85@gmail.com>
2022-03-28 10:04:34 +0000

    Deploying to gh-pages from  @ b314ca7f75d746d4e70b391f0d361b13819710d2 🚀

--- _content/MdIcons.Blazor/MdiIcons.Blazor.js
+++ _content/MdIcons.Blazor/MdiIcons.Blazor.js
@@ -5,12 +5,18 @@ export function loadIconFont() {
 
 
     if (!document.getElementById("mdiicons-blazor-css")) {
+        var baseHref = "/";
+        var base = document.getElementsByTagName("base")[0];
+        if (base) {
+           baseHref= base.getAttribute('href');
+        }
+
         var head = document.getElementsByTagName('head')[0];
         var link = document.createElement('link');
         link.id = "mdiicons-blazor-css";
         link.rel = 'stylesheet';
         link.type = 'text/css';
-        link.href = '/_content/MdIcons.Blazor/lib/@mdi/css/materialdesignicons.min.css';
+        link.href = baseHRef+'_content/MdIcons.Blazor/lib/@mdi/css/materialdesignicons.min.css';
         link.media = 'all';
         head.appendChild(link);
     }
